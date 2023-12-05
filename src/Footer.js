298de39b4376a35244decc3a532c385986e8d03a
Ana2020143716 <anacsrodrigues.46@gmail.com>
2023-12-05 14:59:54 +0000

    footer

---
+++ src/Footer.js
@@ -0,0 +1,36 @@
+//import logo from './logo.svg';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <footer>
+                <div id="barfooter">
+                    <div class="row" id="footer">
+                        <div class="col-lg-5 tab">
+                            <img src="./imagens/sitio.svg" alt="sitio"></img>
+
+                            <p class="textfooter">Localidade, povoação, terreno ou local... ou então
+                                    um projeto que engloba todas as exposições, galerias
+                                    e muito mais de um determinado distrito português!</p>
+                                    
+                                    <p>A bem dita “arte” compactada num só website!</p>
+                        </div>
+        
+                                    <div class="col-lg-3 tab" id="footerContact">
+                                        <img src="./imagens/mail.svg" alt="mail"></img>
+                                            <p>geral@sitio.com</p>
+                                            <img src="./imagens/phone.svg" alt="phone"></img>
+                                                <p>+351987654321</p>
+                                    </div>
+                                    </div>
+                                </div>
+
+                            </footer>
+
+
+
+                        </div>
+                        );
+  }
+
+                        export default Footer;
