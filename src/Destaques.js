002bef238290bd5fb45ddcb504147dbc90417ef6
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-01-05 20:03:35 +0000

    Merge branch 'main' of https://github.com/constancacc/sitio_final

--- src/Destaques.js
+++ src/Destaques.js
@@ -137,7 +137,7 @@ function Destaques() {
                 </div>
             </div> */}
            
-             
+             <Footer />
     </div>
   );
 }
--- src/componentes/Carousel2.js
+++ src/componentes/Carousel2.js
@@ -4,63 +4,82 @@ import image2 from '../imagens/image2.png';
 import image3 from '../imagens/image3.png';
 import image4 from '../imagens/image4.png';
 import image5 from '../imagens/image5.png';
+import { ImageGallery } from "react-image-grid-gallery";
 
-const Carousel = () => {
-    return (
-        <div className="container">
-            <div className='image'>
-                <img src={image1} alt="image1" />
-            </div>
+const imagesArray = [
+  {
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: image1 ,
+  },
+  {
+    alt: "Image2's alt text",
+    caption: "Image2's description",
+    src: image2,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image3,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image4,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image5,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image4,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image5,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image4,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image5,
+  },
+  {
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: image1 ,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image4,
+  },
+  {
+    alt: "Image3's alt text",
+    caption: "Image3's description",
+    src: image5,
+  },
+  {
+    alt: "Image1's alt text",
+    caption: "Image1's description",
+    src: image1 ,
+  },
+];
 
-            <div className='image'>
-                <img src={image4} alt="image4" />
-            </div>
-
-            <div className='image'>
-                <img src={image5} alt="image5" />
-            </div>
-
-            <div className='image'>
-                <img src={image3} alt="image3" />
-            </div>
-
-            <div className='image'>
-                <img src={image1} alt="image1" />
-            </div>
-
-            <div className='image'>
-                <img src={image4} alt="image4" />
-            </div>
-
-            <div className='image'>
-                <img src={image2} alt="image2" />
-            </div>
-
-            <div className='image'>
-                <img src={image3} alt="image3" />
-            </div>
-
-            <div className='image'>
-                <img src={image1} alt="image1" />
-            </div>
-
-            <div className='image'>
-                <img src={image4} alt="image4" />
-            </div>
-            <div className='image'>
-                <img src={image2} alt="image2" />
-            </div>
-
-            <div className='image'>
-                <img src={image3} alt="image3" />
-            </div>
-
-            
-
-   
-
-        </div>
-    )
+export default function App() {
+  return (
+    <ImageGallery
+      imagesInfoArray={imagesArray}
+      columnWidth={200}
+      gapSize={15}
+    />
+  );
 }
-
-export default Carousel;
