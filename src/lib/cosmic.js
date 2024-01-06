5135e2725942cd88fe9036b965bae7988362f102
f_rodrigues <franciscofrodrigues28@gmail.com>
2024-01-06 13:39:23 +0000

    Fetch cosmic

---
+++ src/lib/cosmic.js
@@ -0,0 +1,17 @@
+// Import
+import { createBucketClient } from "@cosmicjs/sdk";
+
+// Authenticate
+const cosmic = createBucketClient({
+  bucketSlug: "sitio-production-f54d2e30-938a-11ee-bad3-c399e8060022",
+  readKey: "Cp7nTIDRVoAA7k6bhFe2nOrRDzhFdYsP7KKUIuqfaBK8fMdlOu",
+  writeKey: "GZBEt3ucsQJ76GkJtRiPxluUVbi7IkgNec6lPhL77NHPpo2tlc",
+});
+
+export async function getAllArtists() {
+  const data = await cosmic.objects
+    .find({ type: "artistas" })
+    .props("id,slug,title,metadata");
+  // .depth(1);
+  return data.objects;
+}
--- src/paginas/Artistas.js
+++ src/paginas/Artistas.js
@@ -1,25 +1,63 @@
+import { useEffect, useState } from "react";
+import { getAllArtists } from "../lib/cosmic.js";
+
 import Menu from "../componentes/Menu.js";
 import Footer from "../componentes/Footer.js";
 
 import "../css/artistas.css";
 
 function Artistas() {
-  const names = [
-    "paula rego",
-    "santiago ribeiro",
-    "vhils",
-    "adriana molder",
-    "julio sarmento",
-    "pedro cabrita reis",
-  ];
+  const [posts, setPosts] = useState([]);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [mousePosition, setMousePosition] = useState({ x: -200, y: -200 });
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const fetchedPosts = await getAllArtists();
+
+        fetchedPosts.forEach((post) => {
+          const img = new Image();
+          img.src = post.metadata.imagem_artista.url;
+        });
+
+        console.log(fetchedPosts);
+
+        setPosts(fetchedPosts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const imagePosition = (event) => {
+    setMousePosition({ x: event.clientX, y: event.clientY });
+  };
 
   return (
-    <div id="PagDestaques">
+    <div id="PagDestaques" onMouseMove={imagePosition}>
       <Menu />
-      {names.map((name) => (
-        <div className="row artista-list-element">
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          className="row artista-list-element"
+          onMouseEnter={() => setHoveredIndex(post.id)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
           <div className="col-xs-12">
-            <h1>{name}</h1>
+            {hoveredIndex === post.id && (
+              <img
+                className="artista-imagem"
+                src={post.metadata.imagem_artista.url}
+                alt="imagem-artista"
+                style={{
+                  top: mousePosition.y,
+                  left: mousePosition.x,
+                }}
+              />
+            )}
+            <h1>{post.title}</h1>
             <hr />
           </div>
         </div>
