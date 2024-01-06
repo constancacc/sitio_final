0e010c69a4bdd8db0ae39fb65e67f1894ee5737b
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-01-06 22:25:17 +0000

    galerias local hora

--- src/paginas/Galerias.js
+++ src/paginas/Galerias.js
@@ -4,12 +4,17 @@ import { getAllGalleries } from "../lib/cosmic.js";
 import Menu from "../componentes/Menu.js";
 import Footer from "../componentes/Footer.js";
 
+import location from '../imagens/location.svg';
+import clock from '../imagens/Clock.svg';
+
 import "../css/galerias.css";
 
 function Galerias() {
   const [posts, setPosts] = useState([]);
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [mousePosition, setMousePosition] = useState({ x: -200, y: -200 });
+  const [clicked, setClicked] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -35,15 +40,20 @@ function Galerias() {
     setMousePosition({ x: event.clientX, y: event.clientY });
   };
 
+  const toggleColor = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <div id="PagDestaques" onMouseMove={imagePosition}>
       <Menu />
       {posts.map((post) => (
         <div
           key={post.id}
-          className="row galeria-list-element"
+          className={`row galeria-list-element ${selectedIndex === post.id ? 'clicked' : ''}`}
           onMouseEnter={() => setHoveredIndex(post.id)}
           onMouseLeave={() => setHoveredIndex(null)}
+          onClick={() => setSelectedIndex(post.id)}
         >
           <div className="col-xs-12">
             {hoveredIndex === post.id && (
@@ -59,7 +69,19 @@ function Galerias() {
             )}
             <h1>{post.title}</h1>
             <hr />
+            {selectedIndex === post.id && (
+              <div className="galeria-list-element-expanded">
+                <div className="galeria-list-content-expanded">
+                <img src={location} alt="location" className="icons"/>
+                  <p>{post.metadata.localizacao_galeria}</p>
+                  <img src={clock} alt="clock" className="icons"/>
+                  <p>{post.metadata.horario_galeria}</p>
+                </div>
+              </div>
+            )}
+
           </div>
+
         </div>
       ))}
       <Footer />
