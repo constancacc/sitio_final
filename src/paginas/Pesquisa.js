e5a4347845706f0a7ee3509637e2667d64a72327
constancacc <constancamfcorreia@gmail.com>
2024-01-07 15:37:37 +0000

    pesquisa completa

--- src/paginas/Pesquisa.js
+++ src/paginas/Pesquisa.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
-import { getAllArtists, getAllGalleries} from "../lib/cosmic.js";
-
-
+import { getAllArtists, getAllGalleries } from "../lib/cosmic.js";
 import Menu from "../componentes/Menu.js";
 import Footer from "../componentes/Footer.js";
-
 import "../css/pesquisa.css";
 
 function Pesquisa() {
   const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -26,30 +28,38 @@ function Pesquisa() {
     fetchData();
   }, []);
 
+  const filteredPosts = posts.filter(post => {
+    const searchTerm = value.toLowerCase();
+    const title = post.title.toLowerCase();
 
-
+    return title.includes(searchTerm);
+  });
 
   return (
     <div id="PagDestaques" >
       <Menu />
-      <div className="row" style={{height: "5vw"}}></div>
       
-      <input 
-        type="text" name="search" id="data-search"
-        size="40" maxlength="256" value=""
-        placeholder="Pesquisa por nome da obra ou tipo de arte"/>
-        
-      {posts.map((post) => (
-        <div
-          key={post.id}
-          className="row list-element"
-        >
-          <div className="col-xs-12">
-            <h1>{post.title}</h1>
-            <hr />
+      <div className="row" style={{ height: "5vw" }}>
+        <input
+          type="text" name="search" id="data-search"
+          size="40" maxLength="256" value={value} onChange={onChange}
+          placeholder="Pesquisa por nome da obra ou tipo de arte" />
+      </div>
+
+      {value && (
+        filteredPosts.map((post) => (
+          <div
+            key={post.id}
+            className="row list-element"
+          >
+            <div className="col-xs-12">
+              <h1>{post.title}</h1>
+              <hr />
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
+
       <Footer />
     </div>
   );
