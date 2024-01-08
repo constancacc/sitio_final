3e78ccff082218aae6e399aedb14c36bf6abac4a
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-01-08 15:48:55 +0000

    artistas destaques

--- src/componentes/Carousel2.js
+++ src/componentes/Carousel2.js
@@ -1,12 +1,8 @@
 import { useEffect, useState } from "react";
 import { getAllArtists } from "../lib/cosmic.js";
-
-import '../css/carousel2.css';
-import { ImageGallery } from "react-image-grid-gallery";
-
+import '../css/carousel2.css'; // Certifique-se de ter estilos adequados para a grade
 
 export default function App() {
-
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
@@ -29,20 +25,28 @@ export default function App() {
 
     return (
         <div className='image-gallery-container'>
-            {posts.map((post) => (
-                <ImageGallery
-                    imagesInfoArray={[
-                        {
-                            src: post.metadata.imagem_artista.url,
-                            thumbnail: post.metadata.imagem_artista.url,
-                            caption: post.title,
-                        }
-                    ]}
-                    columnWidth={200}
-                    gapSize={15}
-                />
-            ))}
-
+            <div className='image-grid'>
+                {posts.map((post) => {
+              
+                    return (
+                        <div
+                            key={post._id}
+                            className='grid-item'
+                        >
+                            <img
+                                src={post.metadata.imagem_artista.url}
+                                alt={post.title}
+                            />
+                            <div className='caption'>
+                                <div className='textCaption'>
+                                    <h5>{post.title}</h5>
+                                    <h6>{post.metadata.idade}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
